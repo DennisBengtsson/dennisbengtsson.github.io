a11fc58f08a93d0abf4909e1e5d549c1bf8a95b5
Dennis Bengtsson <dennisrmgbengtsson@gmail.com>
2025-12-31 11:10:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,12 +46,12 @@
         </div>
     `;
 
-    const footerHTML = `
-        <div class="container py-4 bg-secondary text-center">
-            <p class="m-0 text-white">
-                &copy; 2025 <span id="footer-name">Dennis Bengtsson</span>. Ingen datainsamling. Alla rättigheter förbehållna.
-            </p>
-        </div>
+<div class="container py-4 bg-secondary text-center">
+    <p class="m-0 text-white small">
+        &copy; 2025 <span id="footer-name">Dennis Bengtsson</span><br>
+        Ingen datainsamling. Alla rättigheter förbehållna.
+    </p>
+</div>
     `;
 
     /* =========================================
