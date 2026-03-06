e0515a62345705d5ded02d8fd35f3ba6c3f117bb
Dennis Bengtsson <dennisrmgbengtsson@gmail.com>
2026-03-06 12:53:59 +0100

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -2,9 +2,9 @@
 
 function loadNewsCarousel() {
     const rssUrls = [
-        'https://feeds.expressen.se/nyheter/',
-        'https://www.svt.se/nyheter/rss.xml',
-        'https://www.aftonbladet.se/rss.xml'
+        '',
+        '',
+        ''
     ];
 
     const requests = rssUrls.map(url => {
